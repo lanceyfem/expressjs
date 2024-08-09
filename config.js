8e33f5961e3dfc0d8067824c291931b079dbc2a6
lancey fem <lfdacruz@iskolarngbayan.pup.edu.ph>
2024-08-09 15:10:13 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+module.exports = {
+    nexCloudApiUrl: 'https://select.nextech-api.com/api',
+    justGoingViralApiUrl: 'https://api.justgoingviral.com',
+    secretKey: 'your-secret-key',
+    applicationId: '7d6097cb-f0e7-43f7-a9eb-52ecd1bad6b5',
+    groupId: '49cc8b62-2415-4fc4-9a2d-b2aeefa4f7d5',
+    username: 'd7a6b4f1-3bb0-431b-9077-60ad6b18ed20@nextech-api.com',
+    password: 'iQe}sRu"-I4kK:lS(~yC',
+  };
